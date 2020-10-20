1b4538a3e056b23b23d7132594f478b0d624cbd0
Snoopy <2322168080@qq.com>
2020-10-20 10:14:09 -0400

    Add tank movement

--- TankAI.cpp
+++ TankAI.cpp
@@ -3,25 +3,38 @@
 #include <stdexcept>
 #include <iostream>
 #include <algorithm>
+#include <random>
 
 TankAI::TankAI(Scene& scene, const glm::vec3& initial_pos) :
 tank_(scene, initial_pos, false),
 scene_(scene)
-{}
+{
+	OnMoveEnter();
+}
 
 void TankAI::Update(float elapsed, const glm::vec3& player_pos)
 {
+	constexpr float barrel_pitch { glm::radians(5.0f) };
 	switch (state_) {
-		case State::MOVE:
-			state_ = State::AIM_AND_FIRE;
-		break;
-		case State::AIM_AND_FIRE: {
-			constexpr float barrel_pitch { glm::radians(5.0f) };
-
-			// Move Turret and Barrel
+		case State::MOVE: {
+			sub_movement_.Update(elapsed, tank_);
 
 			tank_.AdjustBarrel(barrel_pitch);
+			float target_yaw = CalcYaw(tank_.GetPosition(), player_pos) - tank_.GetBodyYaw();
+			
+			if (std::abs(target_yaw - tank_.GetTurretYaw()) >= glm::radians(3.0f)) {
+				tank_.TargetTurret(target_yaw);
+			}
 
+			move_time_ -= elapsed;
+			if (move_time_ <= 0.0f) {
+				state_ = State::AIM_AND_FIRE;
+			}
+			break;
+		}
+		case State::AIM_AND_FIRE: {
+			// Move Turret and Barrel
+			tank_.AdjustBarrel(barrel_pitch);
 			float target_yaw = CalcYaw(tank_.GetPosition(), player_pos) - tank_.GetBodyYaw();
 			
 			if (std::abs(target_yaw - tank_.GetTurretYaw()) >= glm::radians(3.0f)) {
@@ -30,6 +43,7 @@ void TankAI::Update(float elapsed, const glm::vec3& player_pos)
 				float shell_speed = CalcShellSpeed(tank_.GetFirePosition(), player_pos, tank_.GetBarrelPitch());
 				if (shell_speed > 0.0f) {
 					tank_.Fire(shell_speed);
+					OnMoveEnter();
 					state_ = State::MOVE;
 				}
 			}
@@ -69,5 +83,79 @@ float TankAI::CalcShellSpeed(const glm::vec3& src_pos, const glm::vec3& dest_pos
 	return target_pos.x / (glm::cos(pitch) * fly_time);
 }
 
+void TankAI::OnMoveEnter()
+{
+	static std::random_device rd;
+	static std::mt19937 mt(rd());
+	static std::uniform_real_distribution<float> move_time_dist(3.0f, 10.0f);
+
+	move_time_ = move_time_dist(mt);
+	sub_movement_.Initialize();
+}
 		
-		
+
+void TankAI::SubMovement::Update(float elapsed, Tank& tank)
+{
+	time_ -= elapsed;
+	if (time_ < 0.0f) {
+		Initialize();
+	}
+
+	if (move_forward_) {
+		tank.MoveForward();
+	}
+
+	if (move_backward_) {
+		tank.MoveBackward();
+	}
+
+	if (turn_left_) {
+		tank.TurnLeft();
+	}
+
+	if (turn_right_) {
+		tank.TurnRight();
+	}
+}
+
+void TankAI::SubMovement::Initialize()
+{
+	static std::random_device rd;
+	static std::mt19937 mt(rd());
+	static std::uniform_int<int> dice_dist(0, 2);
+	static std::uniform_real_distribution<float> time_dist(1.0f, 3.0f);
+
+	time_ = time_dist(mt);
+
+	int dice = dice_dist(mt);
+	switch (dice) {
+		case 0:
+		turn_left_ = false;
+		turn_right_ = false;
+		break;
+		case 1:
+		turn_left_ = true;
+		turn_right_ = false;
+		break;
+		case 2:
+		turn_left_ = false;
+		turn_right_ = true;
+		break;
+	}
+
+	dice = dice_dist(mt);
+	switch (dice) {
+		case 0:
+		move_forward_ = false;
+		move_backward_ = false;
+		break;
+		case 1:
+		move_forward_ = true;
+		move_backward_ = false;
+		break;
+		case 2:
+		move_forward_ = false;
+		move_backward_ = true;
+		break;
+	}
+}
--- TankAI.hpp
+++ TankAI.hpp
@@ -14,6 +14,19 @@ private:
 	Tank tank_;
 	Scene& scene_;
 
+	float move_time_ { 0.0f };
+
+	struct SubMovement {
+		float time_;
+		bool turn_right_;
+		bool turn_left_;
+		bool move_forward_;
+		bool move_backward_;
+
+		void Update(float elapsed, Tank& tank);
+		void Initialize();
+	} sub_movement_;
+
 	enum class State : uint8_t {
 		MOVE = 0,
 		AIM_AND_FIRE,
@@ -21,4 +34,6 @@ private:
 
 	static float CalcYaw(const glm::vec2& pos1, const glm::vec2& pos2);
 	static float CalcShellSpeed(const glm::vec3& src_pos, const glm::vec3& dest_pos, float pitch);
+
+	void OnMoveEnter();
 };
